d67b83b4d7f7fdd043cbc535edc3b20e845ef733
Branimir <10777145+branimir-kirilov@users.noreply.github.com>
2023-10-18 16:18:15 +0300

    More tests

--- deck_test.go
+++ deck_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -29,4 +31,57 @@ func TestDeal(t *testing.T) {
 	if len(r) != 47 {
 		t.Fail()
 	}
+}
+
+func TestWriteToFile(t *testing.T) {
+	fn := "_deckTestsFile"
+	os.Remove(fn)
+
+	deck := newDeck()
+	errWrite := deck.writeToFile(fn)
+
+	if errWrite != nil {
+		t.Error("Error while writing the file!")
+	}
+
+	bs, errRead := os.ReadFile(fn)
+
+	if errRead != nil {
+		t.Error("Error while reading a file from the disk!")
+	}
+
+	s := strings.Split(string(bs), ",")
+
+	if len(s) != 52 {
+		t.Errorf("Expected the length to be 52 but it was %v", len(s))
+	}
+
+	if s[0] != "Ace of Spades" {
+		t.Errorf("Expected the first card to be Ace of Spades but it was %v", s[0])
+	}
+
+	os.Remove(fn)
+}
+
+func TestLoadDeckFromFile(t *testing.T) {
+	fn := "_deckTestsFile"
+	os.Remove(fn)
+
+	d := newDeck()
+	errWrite := d.writeToFile(fn)
+
+	if errWrite != nil {
+		t.Error("Error while writing the file!")
+	}
+
+	deck := loadDeckFromFile(fn)
+	if len(deck) != 52 {
+		t.Errorf("Expected the length to be 52 but it was %v", len(deck))
+	}
+
+	if deck[0] != "Ace of Spades" {
+		t.Errorf("Expected the first card to be Ace of Spades but it was %v", deck[0])
+	}
+
+	os.Remove(fn)
 }
